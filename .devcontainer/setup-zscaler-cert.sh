71ed90049bebc760f212fc2975bd79eee92918e1
Johan Olsson <johan.olsson@non.se.com>
2025-10-19 15:29:14 +0200

    Fix proxy settings
    - Adds a container name.
    - Removes Zscaler certificate configuration as it is no longer needed.
    - Adds environment variables to disable proxy settings within the container.
    - Adds a basic "Hello World" program to test the setup.

--- .devcontainer/setup-zscaler-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Copy the Zscaler root CA certificate to the system's CA certificates directory
-sudo cp ./.devcontainer/ZscalerRootCA.crt /usr/local/share/ca-certificates/ZscalerRootCA.crt
-sudo update-ca-certificates
